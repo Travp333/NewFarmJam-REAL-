5017e733dc2f3a92487bac87634605f93ac93217
Travp333 <Travp333@yahoo.com>
2024-02-07 13:39:50 -0800

    sset up basic traversal to barn interior

---
+++ Assets/RootReferenceHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootReferenceHolder : MonoBehaviour
+{
+	[SerializeField]
+	public GameObject root;
+}
---
+++ Assets/Teleport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	[SerializeField]
+	Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	protected void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player"){
+			Debug.Log("TEST");
+			if(other.gameObject.GetComponent<RootReferenceHolder>()!=null){
+				other.gameObject.GetComponent<RootReferenceHolder>().root.transform.position = destination.position;
+			}
+			
+		}
+	}
+}
