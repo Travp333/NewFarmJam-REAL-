0afa342775c68640c021d638003b349c626de605
Mcdogsman <50517785+Mcdogsman@users.noreply.github.com>
2024-02-29 20:49:57 -0800

    soupbutton

---
+++ Assets/SoupButton.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoupButton : MonoBehaviour
+{
+    [SerializeField]
+    public Item soupitem;
+
+    Inven playerInventory;
+    Inven soupInventory;
+    public void SoupClick() {
+        
+        foreach (Inven i in GameObject.FindObjectsOfType<Inven>())
+        {
+            if (i.gameObject.tag == "Player")
+            {
+                playerInventory = i;
+            }
+            if (i.gameObject.tag == "Cooker")
+            {
+                soupInventory = i;
+            }
+        }
+
+        if (playerInventory != null && soupInventory != null)
+        {
+            int veggieCount = 0;
+
+
+            foreach (ItemStat i in soupInventory.array)
+            {
+                if (i.isVeggie)
+                {
+                    veggieCount++;
+                    Debug.Log(veggieCount + " veggies detected");
+                }
+                else
+                    Debug.Log("Wrong Recipe");
+            }
+
+
+            if (veggieCount  == 3)
+            {
+                
+                playerInventory.SmartPickUp(soupitem);
+
+                //deduct
+                for (int i = 0; i < soupInventory.vSize; i++)
+                {
+                    for (int i2 = 0; i2 < soupInventory.hSize; i2++)
+                    {
+                        if (soupInventory.array[i, i2].isVeggie)
+                        {
+                            soupInventory.DeductOneFromSlot(new string(i + "," + i2));
+                        }
+                    }
+                }
+            }
+        }
+        else Debug.Log("Soup Inventories are null");
+    }
+}
